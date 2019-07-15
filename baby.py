8750bf1a998ab32764c6f2cb7758241ef59fb427
Stella Osunkwo <52019428+Stella-O@users.noreply.github.com>
2019-07-15 01:55:48 -0500

    Add files via upload

---
+++ baby.py
@@ -0,0 +1,22 @@
+from random import choice
+
+questions = ["Why is the sky blue?: ",
+             "Why is there a face on the moon?: ",
+             "Where are all the dinosaurs?: ",
+             "Why are we here?: ",
+             "Why are you smiling?: ",
+             "Why do you like to sing in the shower?: ",
+             "Why do I have to drink milk?: ",
+             "Why can't I eat my toys?: ",
+             "Where do babies come from?: ",
+             "Why do I have to eat vegetables?: "]
+
+question = choice(questions)
+answer = input(question).strip().lower()
+
+while(answer) != "just because":
+    answer = input("why? ").strip().lower()
+
+print("Oh... Okay")
+
+    
